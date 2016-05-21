66e047a18da017383cbc30a6f125d9aebde90b40
devill <devillmeister@gmail.com>
2016-05-21 19:41:04 +0200

    Some logging added

--- player.rb
+++ player.rb
@@ -4,20 +4,35 @@ class Player
   VERSION = 'Ludicrous poker player'
 
   def bet_request(game_state)
+    bet = handle_request(game_state)
+    puts "Final bet: #{bet}"
+    return bet
+  rescue StandardError => e
+    puts e
+    return 0
+  end
+
+  def handle_request(game_state)
+    puts game_state
     hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
     if high_pair?(hole_cards)
+      puts "high pair"
       return 10000
     end
 
-    # if game_state['players'].filter { |player| player['status'] == 'active' }.size == 2
-    #   if ak_suited(hole_cards)
-    #     return 1000
-    #   end
-    #
-    #   if game_state['dealer'] == game_state['in_action'] and small_pot(game_state)
-    #     return game_state['small_blind'] * 4 * 4 * 10
-    #   end
-    # end
+    if game_state['players'].filter { |player| player['status'] == 'active' }.size == 2
+      puts "heads up"
+
+      if ak_suited(hole_cards)
+        puts "ak suited"
+        return 1000
+      end
+
+      if game_state['dealer'] == game_state['in_action'] and small_pot(game_state)
+        puts "late position"
+        return game_state['small_blind'] * 4 * 4 * 10
+      end
+    end
 
     0
   end
