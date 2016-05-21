98f6de42e6de2d9ecdb18152232f6336c83f0c2b
devill <devillmeister@gmail.com>
2016-05-21 18:48:22 +0200

    Bug fixed

--- player.rb
+++ player.rb
@@ -5,7 +5,7 @@ class Player
 
   def bet_request(game_state)
     hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
-    if hole_cards[0]['rank'] == hole_cards[1]['rank']
+    if hole_cards[0]['rank'] == hole_cards[1]['rank'] and [10,'J','Q','K','A'].include?(hole_cards[0]['rank'])
       return 10000
     elsif ['A','K'].include?(hole_cards[0]['rank']) and ['A','K'].include?(hole_cards[1]['rank']) and hole_cards[0]['suit'] == hole_cards[1]['suit']
       return 10000
