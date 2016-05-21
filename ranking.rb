2b716fe61039d8414ace8badca460ec61bc9d288
Akos Maurer-Somogyi <mrakosms@gmail.com>
2016-05-21 22:38:33 +0200

    two pair

--- ranking.rb
+++ ranking.rb
@@ -4,7 +4,7 @@ class Ranking
   end
 
   def pair_with(hole_card, community_cards)
-    community_cards.select { |card| card['rank'] == hole_card['rank'] }.size > 1
+    community_cards.select { |card| card['rank'] == hole_card['rank'] }.size >= 1
   end
 
   def three_of_a_kind_with_our_card(hole_cards, community_cards)
@@ -15,8 +15,15 @@ class Ranking
     community_cards.select { |card| card['rank'] == hole_card['rank'] }.size == 2
   end
 
-  def two_pair_with_with_our_cards(hole_cards, community_cards)
-    pair_with(hole_cards[0], community_cards) && pair_with(hole_cards[1],community_cards)
+  def two_pair_with_our_cards(hole_cards, community_cards)
+    pair_with(hole_cards[0], community_cards) && pair_with(hole_cards[1],community_cards) 
   end
 
+  def two_pair_with_one_of_mine(hole_cards, community_cards)
+    (pair_with(hole_cards[0], community_cards) or pair_with(hole_cards[1],community_cards)) && pair_on_table(community_cards)
+  end
+
+  def pair_on_table(community_cards)
+    community_cards.detect{ |card| community_cards.count(card) > 1 }.size >= 1
+  end
 end
