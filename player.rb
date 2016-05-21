71cd2a23b2125883bd7ec510375d516b29099cfd
devill <devillmeister@gmail.com>
2016-05-21 18:25:05 +0200

    Ludicrous poker player

--- player.rb
+++ player.rb
@@ -1,10 +1,14 @@
 
 class Player
 
-  VERSION = "Default Ruby folding player"
+  VERSION = 'Ludicrous poker player'
 
   def bet_request(game_state)
-    10000
+    hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
+    if hole_cards[0]['rank'] == hole_cards[1]['rank']
+      return 10000
+    end
+    0
   end
 
   def showdown(game_state)
