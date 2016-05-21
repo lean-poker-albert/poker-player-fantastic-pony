3d0c85f80618ca5e51cccddee79b9a7f19c6392d
devill <devillmeister@gmail.com>
2016-05-21 21:43:52 +0200

    Remove unnecessary check

--- ranking.rb
+++ ranking.rb
@@ -1,6 +1,6 @@
 class Ranking
   def pair_with_our_card(hole_cards, community_cards)
-    three_of_a_kind_with(hole_cards[0], community_cards) or three_of_a_kind_with(hole_cards[1],community_cards)
+    pair_with(hole_cards[0], community_cards) or three_of_a_kind_with(hole_cards[1],community_cards)
   end
 
   def pair_with(hole_card, community_cards)
