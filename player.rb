778c4eee2bd30405c1894143f7ea6a1033ec7371
devill <devillmeister@gmail.com>
2016-05-21 18:30:45 +0200

    Go all in on AK suited

--- player.rb
+++ player.rb
@@ -7,6 +7,8 @@ class Player
     hole_cards = game_state['players'][game_state['in_action']]['hole_cards']
     if hole_cards[0]['rank'] == hole_cards[1]['rank']
       return 10000
+    elsif ['A','K'].include?(hole_cards[0]['rank']) and ['A','K'].include?(hole_cards[1]['rank']) and hole_cards[0]['suit'] == hole_cards[1]['suit']
+      return 10000
     end
     0
   end
